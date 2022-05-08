d1c65edca0705182679c19809bb298f2cd12c7fa
aminerakibe <aminerakibe7@gmail.com>
2022-05-08 15:07:57 +0100

    fixed

--- level1/ft_strlen/ft_strlen.c
+++ level1/ft_strlen/ft_strlen.c
@@ -6,12 +6,12 @@
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 15:52:10 by amrakibe          #+#    #+#             */
-/*   Updated: 2022/05/07 17:56:01 by amrakibe         ###   ########.fr       */
+/*   Updated: 2022/05/07 17:56:54 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str)
-{ 
+{
     int i = 0;
     while(str[i])
     {
