fea1ba2551e886ad9c11df7d6c68ae16ca3ad2f1
aminerakibe <aminerakibe7@gmail.com>
2022-05-08 21:27:04 +0100

    fixed

--- level2/ft_atoi/ft_atoi.c
+++ level2/ft_atoi/ft_atoi.c
@@ -6,32 +6,37 @@
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 20:06:13 by amrakibe          #+#    #+#             */
-/*   Updated: 2022/05/08 20:31:08 by amrakibe         ###   ########.fr       */
+/*   Updated: 2022/05/08 21:02:04 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(const char *str){
-    int i = 0;
-    int sign = 1;
-    int result = 0;
+#include<stdio.h>
+#include<stdlib.h>
+
+int ft_atoi(const char *str)
+{
+	int i = 0;
+	int sign = 1;
+	int res = 0;
+	
 	while(str[i] <= 32)
+	i++;
+	if(str[i] == '-')
+	{
+		sign = -1;
 		i++;
-    if(str[i] == '-' )
-    {
-        sign = -1;
-        i++;
-    }
-    else if(str[i] == '+')
-        i++;
-    while(str[i] != '\0')
-    {
-        if(str[i] > '9' || str[i] < '0')
-        {
-            return (0);
-        }
-        result = result * 10 + str[i] - 48;
-        i++;
-    }
-    
-    return (result * sign);
+	}
+	else if(str[i] == '+')
+	i++;
+	while(str[i] < '9' && str[i] > '0')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return(res * sign);
+}
+int main()
+{
+	printf("%d\n", ft_atoi("+-12"));
+	printf("%d\n", atoi("+-12"));
 }
