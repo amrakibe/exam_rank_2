911350929813fb21e04778549918f9f58ff2f10f
aminerakibe <aminerakibe7@gmail.com>
2022-05-09 17:37:12 +0100

    fixed

--- level2/ft_strdup/ft_strdup.c
+++ level2/ft_strdup/ft_strdup.c
@@ -1,43 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcspn.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/08 22:29:11 by amrakibe          #+#    #+#             */
-/*   Updated: 2022/05/09 17:18:12 by amrakibe         ###   ########.fr       */
+/*   Created: 2022/05/09 17:21:37 by amrakibe          #+#    #+#             */
+/*   Updated: 2022/05/09 17:36:44 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stdlib.h>
+#include<string.h>
 #include<stdio.h>
 
-char	ft_strcspn(const char *s, const char *reject)
+char *ft_strdup(char *src)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while(s[i])
-	{
-		j = 0;
-		while(reject[j])
-		{
-			if(s[i] == reject[j])
-			return i;
-			j++;
-		}
-		i++;
-	}
-	while(s[i])
-	{
-		i++;
-	}
-	return(i);
+    int i , j;
+    char *buf;
+    
+    i = 0;
+    j = 0;
+    buf = malloc(sizeof(char *));
+    if(!buf)
+        return(NULL); 
+    while(src[i] != '\0')
+    {
+        buf[j++] = src[i++];
+    }
+    buf[j] = '\0';
+    return(buf);
 }
 int main()
 {
-	printf("%ld\n", ft_strcspn("amrakibe", "k"));
-	printf("%ld", strcspn("amrakibe", "k"));
-	
+    printf("%s\n", ft_strdup("amrakibe"));
+    printf("%s\n", strdup("amrakibe"));
 }
