2e1d9534f1f02e9ac99a7ab96e770499d64fa426
aminerakibe <aminerakibe7@gmail.com>
2022-05-22 18:33:10 +0100

    </>

--- level4/rev_wstr/rev_wstr.c
+++ level4/rev_wstr/rev_wstr.c
@@ -6,27 +6,41 @@
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 16:15:01 by amrakibe          #+#    #+#             */
-/*   Updated: 2022/05/22 16:43:25 by amrakibe         ###   ########.fr       */
+/*   Updated: 2022/05/22 18:30:45 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-int s_len(char *str)
+
+void print_unitl_space(char *str)
 {
-    int i =0;
-    int r = 0;
-    while(str[i])
-    { 
-        //    printf("%c", str[i]);
-        if(str[i] == 32 && (str[i+1] >= 'a' && str[i+1] <= 'z'))
-            r++;
-            i++;
-        }
-        return r;
+    int i = 0;
+    while (str[i] && str[i] != ' ' && str[i] != '\t')
+    {
+        write(1, str + i, 1);
+        i++;
     }
-int main(int argc, char **argv)
+}
+
+int main(int ac, char **av)
 {
-    int len_str = s_len(argv[1])+ 1;
-    char **str = malloc(len_str);
+    if(ac == 2)
+    {
+        char *str = av[1];
+        int len = strlen(str) -1;
+        while(len >= 0)
+        {
+            if (str[len] == ' ' || str[len] == '\t' || len == 0)
+            {
+                print_unitl_space(str + len + (len ? 1 : 0));
+                if (len)
+                    write(1, " ", 1);
+            }
+            len--;
+        }    
+    }
+    write(1, "\n", 1);
 }
