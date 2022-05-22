8862f73d2b161702f655fbcd79d1734e20feb4bd
aminerakibe <aminerakibe7@gmail.com>
2022-05-22 16:43:33 +0100

    </>

--- level4/rev_wstr/rev_wstr.c
+++ level4/rev_wstr/rev_wstr.c
@@ -6,29 +6,27 @@
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 16:15:01 by amrakibe          #+#    #+#             */
-/*   Updated: 2022/05/22 16:42:11 by amrakibe         ###   ########.fr       */
+/*   Updated: 2022/05/22 16:43:25 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
 int s_len(char *str)
- {
-     int i =0;
-     int r = 0;
-     while(str[i])
-       { 
+{
+    int i =0;
+    int r = 0;
+    while(str[i])
+    { 
         //    printf("%c", str[i]);
-           if(str[i] == 32 && (str[i+1] >= 'a' && str[i+1] <= 'z'))
-             r++;
-           i++;
+        if(str[i] == 32 && (str[i+1] >= 'a' && str[i+1] <= 'z'))
+            r++;
+            i++;
         }
         return r;
-     
- }
-
-int main(int argc, char **argv) {
+    }
+int main(int argc, char **argv)
+{
     int len_str = s_len(argv[1])+ 1;
     char **str = malloc(len_str);
-    
 }
